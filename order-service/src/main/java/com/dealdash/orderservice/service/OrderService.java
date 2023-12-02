9b89ae739a517f51c37ae1cbe292e377345d0cce
__MK__ <ms.khlif@gmail.com>
2023-12-02 21:06:33 +0100

    Minor changes

--- order-service/src/main/java/com/dealdash/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/dealdash/orderservice/service/OrderService.java
@@ -8,21 +8,23 @@ import com.dealdash.orderservice.model.OrderLineItems;
 import com.dealdash.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-        List<OrderLineItems> orderLineItemsList =  orderRequest.getOrderLineItemsDtoList()
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDto).toList();
 
@@ -32,7 +34,7 @@ public class OrderService {
 
     }
 
-    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
