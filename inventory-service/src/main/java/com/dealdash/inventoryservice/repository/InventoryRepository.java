1aa00a10fd5fb103e176a6b7dee0467311a7e691
__MK__ <ms.khlif@gmail.com>
2023-12-02 20:45:10 +0100

    Add @Repository

--- inventory-service/src/main/java/com/dealdash/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/dealdash/inventoryservice/repository/InventoryRepository.java
@@ -2,6 +2,8 @@ package com.dealdash.inventoryservice.repository;
 
 import com.dealdash.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 }
