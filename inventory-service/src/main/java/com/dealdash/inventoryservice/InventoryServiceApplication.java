19993c1e0c3c6d7d1b0eab191d7adc4e5b2bcce3
__MK__ <ms.khlif@gmail.com>
2023-12-03 10:10:00 +0100

    Fix minor bug

--- inventory-service/src/main/java/com/dealdash/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/dealdash/inventoryservice/InventoryServiceApplication.java
@@ -23,7 +23,7 @@ public class InventoryServiceApplication {
 
             Inventory inventory1 = new Inventory();
             inventory1.setSkuCode("iphone_11");
-            inventory.setQuantity(0);
+            inventory1.setQuantity(0);
 
             inventoryRepository.save(inventory);
             inventoryRepository.save(inventory1);
