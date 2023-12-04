e7749729f3f68db505c8bce24efe99b6b738c653
__MK__ <ms.khlif@gmail.com>
2023-12-04 10:40:50 +0100

    Add load balancing to eureka clients

--- order-service/src/main/java/com/dealdash/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/dealdash/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,7 +37,7 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
         // Call Inventory Service, and place the order if it exists in the inventory
-        InventoryResponse[] inventoryResponsesArray = webClient.get().uri("http://inventory-service/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
