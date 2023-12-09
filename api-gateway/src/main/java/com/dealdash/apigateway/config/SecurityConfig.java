6e34dd2eb8616efe2737e5c839cda7120dfb0990
__MK__ <ms.khlif@gmail.com>
2023-12-09 10:17:41 +0100

    Fix typo

--- api-gateway/src/main/java/com/dealdash/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/dealdash/apigateway/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecuriyConfig {
+public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
