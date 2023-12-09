967aca59e0c1f30d3c0e5f8ca68ded270e837053
__MK__ <ms.khlif@gmail.com>
2023-12-09 11:11:37 +0100

    Implement Resilience4j

--- inventory-service/src/main/java/com/dealdash/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/dealdash/inventoryservice/service/InventoryService.java
@@ -3,18 +3,22 @@ package com.dealdash.inventoryservice.service;
 import com.dealdash.inventoryservice.dto.InventoryResponse;
 import com.dealdash.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
-    // @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> listOfSkuCodes) {
+    public List<InventoryResponse> isInStock(List<String> listOfSkuCodes) throws InterruptedException {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Started");
         return inventoryRepository.findBySkuCodeIn(listOfSkuCodes).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
