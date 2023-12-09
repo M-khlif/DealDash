7e747afadbfd1eee238cbe5fdab62e41ee4a5d5a
__MK__ <ms.khlif@gmail.com>
2023-12-09 09:53:59 +0100

    Add security config class for Keycloak and jwt authentication

---
+++ api-gateway/src/main/java/com/dealdash/apigateway/config/SecuriyConfig.java
@@ -0,0 +1,29 @@
+package com.dealdash.apigateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecuriyConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+}
